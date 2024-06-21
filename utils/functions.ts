4ff887ad48cbf3bfc55098e04f3af525d4fe700e
Anton Agaronov <anton.agaronov@legalbet.com>
2024-06-21 15:05:11 +0400

    feat: add global notify in clientFetch

--- utils/functions.ts
+++ utils/functions.ts
@@ -1,5 +1,21 @@
 import type { NitroFetchOptions } from 'nitropack';
+import type { AjaxResponse } from '~/types/AjaxResponse';
 
 export const clientFetch = async (url: string, options: NitroFetchOptions<any>) => {
-  return await $fetch(url, options).catch((e) => console.error(e));
+  const messageBuilder = useToast();
+  const res =  await $fetch(url, {
+    ...options
+  }).catch((e) => {
+    if (e.data.message) {
+      messageBuilder.add({ title: e.data.message, color: 'red' });
+    }
+  });
+
+  const data = res as AjaxResponse;
+  
+  if (data.message) {
+    messageBuilder.add({ title: data.message, color: 'green' });
+  }
+  console.log(data);
+  return data;
 };
