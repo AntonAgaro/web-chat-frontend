3cc61d1d620b2585b3550de32d7c7dd5a674859d
Anton Agaronov <anton.agaronov@legalbet.com>
2024-06-22 11:17:10 +0400

    feat: refactor auth middleware

--- middleware/01.auth.global.ts
+++ middleware/01.auth.global.ts
@@ -1,19 +1,23 @@
 export default defineNuxtRouteMiddleware(async (to) => {
-  if (import.meta.client || to.path === '/') return;
+  if (import.meta.client) return;
 
   const token = useCookie('token');
   const messageBuilder = useToast();
 
-  if (!token.value)  {
+  if (!token.value && to.path !== '/')  {
     return navigateTo('/');
   }
+
   const authState = useAuthState();
   const authController = useAuthController();
-  const data = await  authController.getUserDetails(token.value);
+  const data = await  authController.getUserDetails(token.value ?? '');
 
   if (data.user) {
     authState.value.user = data.user;
-  } else {
+    if (to.path === '/') {
+      return navigateTo('/chats');
+    }
+  } else if (to.path !== '/') {
     messageBuilder.add({ title: 'User is not authorized!', color: 'red' });
     return navigateTo('/');
   }
