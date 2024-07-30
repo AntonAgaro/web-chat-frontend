0b242e736c9c3fd4aed1ad42d730435264499b61
Anton Agaronov <anton.agaronov@legalbet.com>
2024-07-30 23:12:21 +0400

    feat: prepare chat interface

--- components/ChatCard.vue
+++ components/ChatCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="chat-card pt-2 pl-2 pr-2 flex w-full items-center" data-testid="chat-card">
-    <div class="logo shrink-0 mr-1 w-12 h-12 rounded-full bg-white"/>
+    <div class="logo shrink-0 mr-1 w-12 h-12 rounded-full bg-black  dark:bg-white"/>
     <div class="wrapper pb-2 border-b border-gray-700 gap-1 w-full flex flex-col">
       <div class="top flex justify-between">
         <div class="name">Chat 1</div>
--- pages/chats.vue
+++ pages/chats.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="chats-page border-gray-700	border flex h-full" data-testid="chats-page">
-    <div class="chats-list pt-2 pb-2 border-r border-gray-700 basis-1/3">
+    <div class="chats-list pt-2 pb-2 border-r border-gray-700 max-md:basis-full basis-1/3" :class="{'max-md:hidden': activeChatId}">
       <h3>{{ $t('Chats list') }}</h3>
       <div class="list flex flex-col">
-        <ChatCard/>
-        <ChatCard/>
-        <ChatCard/>
+        <ChatCard v-for="chat in chatsMock" :key="chat.id" @click="activeChatId = chat.id"/>
       </div>
     </div>
-    <div class="chat basis-2/3">
-      <h2>Choose chat!</h2>
+    <div class="chat max-md:basis-full basis-2/3" :class="{'max-md:hidden': !activeChatId}">
+      <UIcon name="i-heroicons-arrow-left md:hidden" class="w-5 h-5" @click="activeChatId = null" />
+      <h2 v-if="!activeChatId">{{ $t('Choose chat')! }}</h2>
+      <h2 v-else> {{ activeChatId }} </h2>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import ChatCard from '~/components/ChatCard.vue';
+const chatsMock = ref([
+  { id: 1 },  { id: 2 },  { id: 3 }
+]);
+const activeChatId = ref<number | null>(null);
 
 const authState = useAuthState();
 console.log('from chats: ', authState.value.user);
